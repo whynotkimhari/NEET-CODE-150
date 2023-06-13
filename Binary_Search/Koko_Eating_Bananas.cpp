9cf10fb67705a09888ca438a3f2cad5369d7e7cb
whynotkimhari <108981453+whynotkimhari@users.noreply.github.com>
2023-06-13 20:34:37 +0700

    Update Koko_Eating_Bananas.cpp

--- Binary_Search/Koko_Eating_Bananas.cpp
+++ Binary_Search/Koko_Eating_Bananas.cpp
@@ -4,22 +4,20 @@ public:
         int ans = INT_MAX;
         sort(piles.begin(), piles.end());
 
-        
-            int start = 1, end = piles[piles.size() - 1];
-            while(start <= end) {
-                int mid = (start + end) / 2;
-                
-                unsigned int hours = 0;
-                for(int i = 0; i < piles.size(); i++) {
-                    hours = hours + ceil(piles[i] * 1.0 / mid);
-                }
-                if(hours > h) start = mid + 1;
-                else {
-                    ans = min(ans, mid);
-                    end = mid - 1;
-                }
+        int start = 1, end = piles[piles.size() - 1];
+        while(start <= end) {
+            int mid = (start + end) / 2;
+
+            unsigned int hours = 0;
+            for(int i = 0; i < piles.size(); i++) {
+                hours = hours + ceil(piles[i] * 1.0 / mid);
             }
-        
+            if(hours > h) start = mid + 1;
+            else {
+                ans = min(ans, mid);
+                end = mid - 1;
+            }
+        }
         return ans;
     }
 };
