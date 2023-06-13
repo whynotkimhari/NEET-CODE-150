67c6c2ea77377faecb6e206c736fd70baace6599
whynotkimhari <108981453+whynotkimhari@users.noreply.github.com>
2023-06-13 19:04:54 +0700

    Update Generate_Parentheses.cpp

--- Stack/Generate_Parentheses.cpp
+++ Stack/Generate_Parentheses.cpp
@@ -4,23 +4,12 @@ public:
     bool isValid(string s) {
         stack<char> st;
         for(int i = 0; i < s.size(); i++) {
-            if(s[i] == '(' || s[i] == '{' || s[i] == '[') {
+            if(s[i] == '(') {
                 st.push(s[i]);
             }
             if(s[i] == ')') {
                 if(st.size() == 0) return false;
                 if(st.top() == '(') st.pop();
-                else if(st.top() == '{' || st.top() == '[') return false;
-            }
-            if(s[i] == '}') {
-                if(st.size() == 0) return false;
-                if(st.top() == '{') st.pop();
-                else if(st.top() == '(' || st.top() == '[') return false;
-            }
-            if(s[i] == ']') {
-                if(st.size() == 0) return false;
-                if(st.top() == '[') st.pop();
-                else if(st.top() == '{' || st.top() == '(') return false;
             }
         }
         if(st.size() != 0) return false;
@@ -40,5 +29,35 @@ public:
             if(isValid(s)) ans.push_back(s);
         }
         return ans;
+    }
+};
+
+## Solution 2: better than 1
+class Solution {
+private:
+    vector<string> ans;
+    string st;
+
+public:
+    void backTracking(int open, int close, int n) {
+        if(open == close && close == n) {
+            this->ans.push_back(st);
+            return;
+        }
+        if(open < n) {
+            this->st.push_back('(');
+            backTracking(open + 1, close, n);
+            this->st.pop_back();
+        }
+        if(open > close) {
+            this->st.push_back(')');
+            backTracking(open, close + 1, n);
+            this->st.pop_back();
+        }
+    }
+
+    vector<string> generateParenthesis(int n) {
+        backTracking(0, 0, n);
+        return this->ans;
     }
 };
