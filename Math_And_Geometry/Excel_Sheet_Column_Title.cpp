d42b53936cc1fa9d833a15243bf99317140895a2
whynotkimhari <108981453+whynotkimhari@users.noreply.github.com>
2023-09-04 14:18:31 +0700

    Create Excel_Sheet_Column_Title.cpp

---
+++ Math_And_Geometry/Excel_Sheet_Column_Title.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    string convertToTitle(int columnNumber) {
+        map<int,char> MMap = {
+            {0, 'Z'},
+            {1, 'A'},
+            {2, 'B'},
+            {3, 'C'},
+            {4, 'D'},
+            {5, 'E'},
+            {6, 'F'},
+            {7, 'G'},
+            {8, 'H'},
+            {9, 'I'},
+            {10, 'J'},
+            {11, 'K'},
+            {12, 'L'},
+            {13, 'M'},
+            {14, 'N'},
+            {15, 'O'},
+            {16, 'P'},
+            {17, 'Q'},
+            {18, 'R'},
+            {19, 'S'},
+            {20, 'T'},
+            {21, 'U'},
+            {22, 'V'},
+            {23, 'W'},
+            {24, 'X'},
+            {25, 'Y'},
+        };
+
+        string answer = "";
+        while(columnNumber) {
+            auto mod = columnNumber % 26;
+            if(mod) columnNumber /= 26;
+            else columnNumber = columnNumber / 26 - 1;
+            answer = MMap[mod] + answer;
+            
+        }
+        return answer;
+    }
+};
