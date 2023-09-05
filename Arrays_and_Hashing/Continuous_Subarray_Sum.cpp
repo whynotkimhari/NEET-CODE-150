6bf19a32f824efb2c706014ed62be89a059b92a3
whynotkimhari <108981453+whynotkimhari@users.noreply.github.com>
2023-09-05 14:40:05 +0700

    Update Continuous_Subarray_Sum.cpp

--- Arrays_and_Hashing/Continuous_Subarray_Sum.cpp
+++ Arrays_and_Hashing/Continuous_Subarray_Sum.cpp
@@ -1,3 +1,30 @@
+// Time O(n^2) Space O(n)
+// Using prefix and nested loop to calculate subarray sum -> TLE 
+class Solution {
+public:
+    bool checkSubarraySum(vector<int>& nums, int k) {
+        if(nums.size() < 2) return false;
+        vector<int> pre(nums.size());
+        pre[0] = nums[0];
+        for(int i = 1; i < nums.size(); i++) {
+            pre[i] = pre[i-1] + nums[i];
+            if(!(pre[i] % k)) return true;
+            for(int j = 0; j < i - 1; j++) {
+                int tmp = pre[i] - pre[j];
+                if(!(tmp % k)) return true;
+            }
+        }
+
+        return false;
+    }
+};
+
+// Time O(n) Space O(n)
+// Using prefix and HashMap to store { remainder(key) : index(value) } of prefix[i] % k
+// Every time a new calculated key has existed in HashMap
+// It means that "" new_sum = last_sum + k*n ""
+// Because new_sum % k == last_sum % k (k * n % k = 0)
+// So that we will find out the answer
 class Solution {
 public:
     bool checkSubarraySum(vector<int>& nums, int k) {
