73e3ae324bf70dcb17a9454a211e8aa25175bdc5
whynotkimhari <htlulem185@gmail.com>
2024-04-02 18:32:09 +0200

    Create Merge_Intervals.py

---
+++ Intervals/Merge_Intervals.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def merge(self, intervals):
+        """
+        :type intervals: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        intervals.sort()
+        q = []
+        maxB, minB = intervals[0][1], intervals[0][0]
+
+        for interval in intervals:
+            if interval[1] < minB or maxB < interval[0]:
+                q.append([minB, maxB])
+                maxB, minB = interval[1], interval[0]
+                    
+            else:
+                maxB = max(maxB, interval[1])
+                minB = min(minB, interval[0])
+
+        q.append([minB, maxB])
+        return q
+        
+        
